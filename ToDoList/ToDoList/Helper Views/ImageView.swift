6f588577966b25955953031b2b0b0addcce92c4f
Timur Dolotkazin <me@tuxracer.ru>
2022-05-30 23:00:43 +0300

    Refactor

--- ToDoList/ToDoList/Helper Views/ImageView.swift
+++ ToDoList/ToDoList/Helper Views/ImageView.swift
@@ -1,36 +1,34 @@
 import SwiftUI
 
 struct ImageView: View {
-
+    
     @Binding var image: UIImage?
     @Binding var isEditMode: Bool
-
+    
     var body: some View {
         if let image = image {
-            Section {
-                HStack {
-                    Spacer()
-                    ZStack(alignment: .topTrailing) {
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
-                            .border(.gray, width: 1)
-                        if isEditMode {
-                            Button {
-                                self.image = nil
-                            } label: {
-                                Image(systemName: "xmark.circle.fill")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 20))
-                                    .offset(x: 10, y: -10)
-                            }
-                            .padding(10)
+            HStack {
+                Spacer()
+                ZStack(alignment: .topTrailing) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .border(.gray, width: 1)
+                    if isEditMode {
+                        Button {
+                            self.image = nil
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.gray)
+                                .font(.system(size: 20))
+                                .offset(x: 10, y: -10)
                         }
+                        .padding(10)
                     }
-                    Spacer()
                 }
-            }
+                Spacer()
+            }            
         }
     }
 }
